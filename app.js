b899f1b1a58d3c4c1c879528b233be1ac7caab8b
Abel <abelalex@live.com>
2020-08-10 02:18:44 -0400

    clean up

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ class GitHub {
         // retrieve user profile and repo
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repo_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        
         // store user profile and repo as JSON
         const profile = await profileResponse.json();
         const repos = await repoResponse.json();
